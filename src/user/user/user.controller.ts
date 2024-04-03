cb93a491a63cdbcd317de0d53ebd7649d01a2c55
rizal suryawan <rizalsuryawan86@gmail.com>
2024-04-03 10:56:46 +0700

    feat: add http request

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('/api/users')
 export class UserController {
+  @Get('/hello')
+  sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName,
+  ): string {
+    return `Hello ${firstName} ${lastName}`;
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string): string {
+    return `GET ${id}`;
+  }
+
   @Post()
   post(): string {
     return 'POST';
